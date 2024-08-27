db8e9bbcc66db0aac563920a43a1490bef997045
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2024-08-27 23:49:10 +0200

    feat(#5): add meida preview

--- frontend/upload_view.js
+++ frontend/upload_view.js
@@ -1,8 +1,36 @@
 import { postMedia, getMedia } from "./upload.js";
 
+/**
+ * @type {HTMLFormElement}
+ * */
 const mediaForm = document.getElementById("mediaForm");
+
+/**
+ * @type {HTMLFormElement}
+ */
 const overlayMeidaForm = document.getElementById("overlayMediaForm");
+
+/**
+ * @type {HTMLButtonElement}
+ */
 const submitButton = document.getElementById("submitButton");
+
+/**
+ * @type {HTMLInputElement}
+ */
+const fileInput = document.getElementById("fileInput");
+
+/**
+ * @type {HTMLImageElement}
+ */
+const mediaPreview = document.querySelector("#image");
+
+fileInput.addEventListener("change", (event) => {
+  if (event?.target?.files && event.target.files[0]) {
+    console.log("loading file");
+    mediaPreview.src = URL.createObjectURL(event.target.files[0]);
+  }
+});
 
 const formData = new FormData(mediaForm);
 
