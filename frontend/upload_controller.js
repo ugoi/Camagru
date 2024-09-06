7e0b202468f4ca210b91eb3d1d0eb861aac59543
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2024-09-06 20:25:32 +0200

    feat(#5): refactor and add pagination to forntend

--- frontend/upload_controller.js
+++ frontend/upload_controller.js
@@ -10,49 +10,9 @@ import {
 } from "./upload_model.js";
 
 /**
- * @type {HTMLFormElement}
- * */
-const mediaForm = document.getElementById("mediaForm");
-
-/**
- * @type {HTMLFormElement}
- */
-const overlayMediaForm = document.getElementById("overlayMediaForm");
-
-/**
- * @type {HTMLButtonElement}
- */
-const submitButton = document.getElementById("submitButton");
-
-/**
- * @type {HTMLButtonElement}
- */
-const cancelButton = document.getElementById("cancelButton");
-
-/**
- * @type {HTMLInputElement}
- */
-const fileInput = document.getElementById("fileInput");
-
-/**
- * @type {HTMLInputElement}
- */
-const overlayFileInput = document.getElementById("overlayFileInput");
-
-/**
- * @type {HTMLImageElement}
- */
-const photo = document.querySelector("#image");
-
-/**
  * @type {HTMLVideoElement}
  */
-const outputVideo = document.querySelector("#outputVideo");
-
-/**
- * @type {HTMLVideoElement}
- */
-var video = document.querySelector("#videoElement");
+var videoPreview = document.querySelector("#videoElement");
 
 /**
  * @type {HTMLDivElement}
@@ -60,22 +20,116 @@ var video = document.querySelector("#videoElement");
 var cameraPreview = document.querySelector("#cameraPreview");
 
 /**
- * @type {HTMLButtonElement}
+ * @type {Boolean}
  */
-var photoButton = document.querySelector("#photoButton");
+var isRecording = false;
+/**
+ *
+ * @param {Boolean} value
+ */
+function setIsRecording(value) {
+  isRecording = value;
+  renderIsRecording(value);
+}
+/**
+ *
+ * @param {Boolean} value
+ */
+function renderIsRecording(value) {
+  const videoButton = document.querySelector("#videoButton");
+  if (value) {
+    videoButton.innerText = "Stop recording";
+  } else {
+    videoButton.innerText = "Start recording";
+  }
+}
 
 /**
- * @type {HTMLButtonElement}
+ * @type {Blob}
  */
-var videoButton = document.querySelector("#videoButton");
+var media = null;
+/**
+ * @param {Blob} value
+ * @returns {void}
+ */
+function setMedia(value) {
+  media = value;
+  renderMedia(value);
+}
+/**
+ * @param {Blob} value
+ * @returns {void}
+ */
+function renderMedia(value) {
+  /**
+   * @type {HTMLImageElement}
+   */
+  const outputPhoto = document.querySelector("#image");
 
-const formData = new FormData();
+  /**
+   * @type {HTMLVideoElement}
+   */
+  const outputVideo = document.querySelector("#outputVideo");
 
-var isRecording = false;
+  if (value == null) {
+    cameraPreview.style.display = "";
+    outputPhoto.style.display = "none";
+    outputPhoto.src = "";
+    outputVideo.style.display = "none";
+    outputVideo.src = "";
+  } else {
+    cameraPreview.style.display = "none";
+    var isVideo = checkIsVideo(value);
+    if (isVideo) {
+      outputPhoto.style.display = "none";
+      outputPhoto.src = "";
+      outputVideo.style.display = "";
+      outputVideo.src = URL.createObjectURL(value);
+    } else {
+      outputVideo.style.display = "none";
+      outputVideo.src = "";
+      outputPhoto.style.display = "";
+      outputPhoto.src = URL.createObjectURL(value);
+    }
+  }
+}
 
+/**
+ * @type {Blob}
+ */
+var overlayMedia = null;
+/**
+ * @param {Blob} value
+ * @returns {void}
+ */
+function setOverlayMedia(value) {
+  overlayMedia = value;
+}
+
+/**
+ * @type {string | null}
+ */
 var containerId = null;
-
-var publishButton = document.getElementById("publishButton");
+/**
+ * @param {string | null} value
+ * @returns {void}
+ */
+function setContainerId(value) {
+  containerId = value;
+  renderContainerId(value);
+}
+/**
+ * @param {string | null} value
+ * @returns {void}
+ */
+function renderContainerId(value) {
+  const publishButton = document.getElementById("publishButton");
+  if (value == null) {
+    publishButton.style.display = "none";
+  } else {
+    publishButton.style.display = "";
+  }
+}
 
 /**
  * @typedef {Object} ImageObject
@@ -88,43 +142,41 @@ var publishButton = document.getElementById("publishButton");
  */
 var userMedia = [];
 
-var overlayAssetPaths = [
-  "assets/overlay/clownfish.png",
-  "assets/overlay/goat.png",
-  "assets/overlay/police-armor.png",
-  "assets/overlay/red-rocket.png",
-];
+// Load overlay assets
+(async () => {
+  var overlayAssetPaths = [
+    "assets/overlay/clownfish.png",
+    "assets/overlay/goat.png",
+    "assets/overlay/police-armor.png",
+    "assets/overlay/red-rocket.png",
+  ];
+  // Add overlay assets to the carousel
+  if (overlayAssetPaths && overlayAssetPaths.length > 0) {
+    for (let index = 0; index < overlayAssetPaths.length; index++) {
+      let path = overlayAssetPaths[index];
+      let element = document.createElement("img");
+      element.src = path;
+      element.className = "carousel-img";
+      document.getElementById("logos-slide").appendChild(element);
 
-// Add overlay assets to the carousel
-if (overlayAssetPaths && overlayAssetPaths.length > 0) {
-  for (let index = 0; index < overlayAssetPaths.length; index++) {
-    let path = overlayAssetPaths[index];
-    let element = document.createElement("img");
-    element.src = path;
-    element.className = "carousel-img";
-    document.getElementById("logos-slide").appendChild(element);
-
-    element.addEventListener("click", async (event) => {
-      const result = await fetch(path);
-      const blob = await result.blob();
-      console.log(blob);
-      formData.delete("overlayMedia");
-      formData.append("overlayMedia", blob);
-      var fileInput = document.getElementById("overlayFileInput");
-      fileInput.value = "";
-    });
+      element.addEventListener("click", async (event) => {
+        const result = await fetch(path);
+        const blob = await result.blob();
+        setOverlayMedia(blob);
+      });
+    }
+  } else {
+    console.log("No overlay assets found");
   }
-} else {
-  console.log("No overlay assets found");
-}
+})();
 
 //Start camera preview
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices
     .getUserMedia({ video: true })
     .then(function (stream) {
-      video.srcObject = stream;
-      video.play();
+      videoPreview.srcObject = stream;
+      videoPreview.play();
     })
     .catch(function (error) {
       console.log("Something went wrong!");
@@ -132,138 +184,71 @@ if (navigator.mediaDevices.getUserMedia) {
     });
 }
 
-async function loadUserMedia() {
-  console.log("Getting user media");
+var after = null;
 
-  var myMedia = document.getElementById("myMedia");
+async function reloadUserMedia() {
+  after = null;
+  const myMedia = document.getElementById("myMedia");
+  myMedia.innerHTML = "";
+  await loadNextUserMedia();
+}
 
-  console.log("myMedia", myMedia);
-
-  const result = await getUserMedia(null, 10);
-
-  console.log("result", result);
-
+/**
+ * @param {String} after
+ * @returns {Promise<void>}
+ */
+async function loadNextUserMedia() {
+  const myMedia = document.getElementById("myMedia");
+  const result = await getUserMedia(after, 3);
   const json = await result.json();
-
-  console.log("json", json);
-
   const data = json.data;
-
+  after = json.paging.after;
   userMedia = data;
+
+  console.log("After: ", after);
+
+  if (after == null) {
+    document.getElementById("loadMoreButton").style.display = "none";
+  } else {
+    document.getElementById("loadMoreButton").style.display = "";
+  }
 
   if (data && data.length > 0) {
     // Remove the previous images
-    myMedia.innerHTML = "";
     for (let index = 0; index < data.length; index++) {
-      let mediaElement = data[index];
-
+      const mediaElement = data[index];
       const downloadUrl = mediaElement.downloadUrl;
       const fileType = await checkFileType(downloadUrl);
-
-      console.log("fileType", fileType);
-
       const isVideo = fileType === "video";
-
       const objectURL = downloadUrl;
-      // const isVideo = checkIsVideo(blob);
-
       if (isVideo) {
-        //TODO: Extract this to a function
-        var divWrapper = document.createElement("div");
-        var outputVideo = document.createElement("video");
-        var deleteButton = document.createElement("button");
-        divWrapper.style.paddingBottom = "10px";
-        outputVideo.src = objectURL;
-        outputVideo.controls = true;
-        outputVideo.className = "captured-video";
-        deleteButton.innerText = "Delete";
-        deleteButton.id = "deleteButton";
-        deleteButton.addEventListener("click", async (event) => {
-          await deleteMedia(mediaElement.id);
-          loadUserMedia();
-        });
-
-        divWrapper.appendChild(outputVideo);
-        divWrapper.appendChild(deleteButton);
-
-        myMedia.appendChild(divWrapper);
+        var outputMedia = document.createElement("video");
       } else {
-        var divWrapper = document.createElement("div");
-        var myImage = document.createElement("img");
-        var deleteButton = document.createElement("button");
-        divWrapper.style.paddingBottom = "10px";
-        myImage.src = objectURL;
-        myImage.className = "captured-img";
-        deleteButton.innerText = "Delete";
-        deleteButton.id = "deleteButton";
-        deleteButton.addEventListener("click", async (event) => {
-          await deleteMedia(mediaElement.id);
-          loadUserMedia();
-        });
-
-        divWrapper.appendChild(myImage);
-        divWrapper.appendChild(deleteButton);
-
-        myMedia.appendChild(divWrapper);
+        var outputMedia = document.createElement("img");
       }
+      const divWrapper = document.createElement("div");
+      const deleteButton = document.createElement("button");
+      divWrapper.style.paddingBottom = "10px";
+      divWrapper.id = mediaElement.id;
+      outputMedia.src = objectURL;
+      outputMedia.controls = true;
+      outputMedia.className = "captured-media";
+      deleteButton.innerText = "Delete";
+      deleteButton.id = "deleteButton";
+      deleteButton.addEventListener("click", async (event) => {
+        try {
+          await deleteMedia(mediaElement.id);
+          myMedia.removeChild(divWrapper);
+        } catch (error) {
+          console.error("Failed to delete media", error);
+        }
+      });
+      divWrapper.appendChild(outputMedia);
+      divWrapper.appendChild(deleteButton);
+      myMedia.appendChild(divWrapper);
     }
   }
 }
-
-// async function loadUserMedia() {
-//   console.log("Getting user media");
-
-//   var myMedia = document.getElementById("myMedia");
-
-//   console.log("myMedia", myMedia);
-
-//   const blobs = await getServeUserMedia();
-
-//   console.log("blobs", blobs);
-
-//   if (blobs && blobs.length > 0) {
-//     // Remove the previous images
-//     myMedia.innerHTML = "";
-//     for (let index = 0; index < blobs.length; index++) {
-//       const blob = blobs[index];
-//       const objectURL = URL.createObjectURL(blob);
-//       const isVideo = checkIsVideo(blob);
-
-//       if (isVideo) {
-//         //TODO: Extract this to a function
-//         var divWrapper = document.createElement("div");
-//         var outputVideo = document.createElement("video");
-//         var deleteButton = document.createElement("button");
-//         divWrapper.style.paddingBottom = "10px";
-//         outputVideo.src = objectURL;
-//         outputVideo.controls = true;
-//         outputVideo.className = "captured-video";
-//         deleteButton.innerText = "Delete";
-//         deleteButton.id = "deleteButton";
-//         deleteButton.addEventListener("click", async (event) => {});
-
-//         divWrapper.appendChild(outputVideo);
-//         divWrapper.appendChild(deleteButton);
-
-//         myMedia.appendChild(divWrapper);
-//       } else {
-//         var divWrapper = document.createElement("div");
-//         var myImage = document.createElement("img");
-//         var deleteButton = document.createElement("button");
-//         divWrapper.style.paddingBottom = "10px";
-//         myImage.src = objectURL;
-//         myImage.className = "captured-img";
-//         deleteButton.innerText = "Delete";
-//         deleteButton.id = "deleteButton";
-
-//         divWrapper.appendChild(myImage);
-//         divWrapper.appendChild(deleteButton);
-
-//         myMedia.appendChild(divWrapper);
-//       }
-//     }
-//   }
-// }
 
 // Load user media
 window.addEventListener("DOMContentLoaded", async (event) => {
@@ -272,189 +257,113 @@ window.addEventListener("DOMContentLoaded", async (event) => {
 
   console.log("Done waiting");
 
-  loadUserMedia();
+  reloadUserMedia();
+});
+
+document.getElementById("loadMoreButton").addEventListener("click", (event) => {
+  loadNextUserMedia();
 });
 
 //Event listeners
-fileInput.addEventListener("change", (event) => {
+document.getElementById("fileInput").addEventListener("change", (event) => {
   if (event?.target?.files && event.target.files[0]) {
     var blob = event.target.files[0]; // See step 1 above
-    formData.delete("media");
-    formData.append("media", blob);
-    console.log("blob type: ", blob.type);
-    /**
-     * @type {String}
-     */
-    cameraPreview.style.display = "none";
-    var isVideo = checkIsVideo(blob);
-    console.log("isVideo", isVideo);
+    setMedia(blob);
+  }
+});
 
-    if (isVideo) {
-      photo.style.display = "none";
-      outputVideo.style.display = "";
-      outputVideo.src = URL.createObjectURL(event.target.files[0]);
-    } else {
-      outputVideo.style.display = "none";
-      photo.style.display = "";
-      photo.src = URL.createObjectURL(event.target.files[0]);
+document
+  .getElementById("overlayFileInput")
+  .addEventListener("change", (event) => {
+    if (event?.target?.files && event.target.files[0]) {
+      var blob = event.target.files[0];
+      setOverlayMedia(blob);
     }
-  }
-});
+  });
 
-overlayFileInput.addEventListener("change", (event) => {
-  if (event?.target?.files && event.target.files[0]) {
-    var blob = event.target.files[0];
-    /**
-     * @type {String}
-     */
-    formData.delete("overlayMedia");
-    formData.append("overlayMedia", blob);
-  }
-});
-
-publishButton.addEventListener("click", async (event) => {
-  event.preventDefault();
-  if (containerId != null) {
-    var response = await publishMedia(containerId);
-    if (response.status === 200) {
-      console.log("Media published");
-      //TODO: Extract this to a function like setContainerID()
-      containerId = null;
-      publishButton.style.display = "none";
-      //
-      loadUserMedia();
-    } else {
-      console.log("Failed to publish media");
+document
+  .getElementById("publishButton")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+    if (containerId != null) {
+      var response = await publishMedia(containerId);
+      if (response.status === 200) {
+        console.log("Media published");
+        setContainerId(null);
+        reloadUserMedia();
+      } else {
+        console.log("Failed to publish media");
+      }
     }
-  }
-});
+  });
 
-submitButton.addEventListener("click", async (event) => {
+document
+  .getElementById("submitButton")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("media", media);
+    formData.append("overlayMedia", overlayMedia);
+
+    // Display the key/value pairs
+    console.log("Sart logging");
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ", " + pair[1]);
+    }
+
+    var postResponse = await postMedia(formData);
+    var postJson = await postResponse.json();
+    var id = postJson.containerId;
+    setContainerId(id);
+    var response = await getServeMedia(id);
+    var myBlob = await response.blob();
+    setMedia(myBlob);
+  });
+
+document.getElementById("cancelButton").addEventListener("click", (event) => {
   event.preventDefault();
-  cameraPreview.style.display = "display";
-
-  if (overlayMediaForm == null || mediaForm == null) {
-    throw new Error("Overlay media form or media form is null");
-  }
-
-  const overlayMediaFormData = new FormData(overlayMediaForm);
-  const mediaFormData = new FormData(mediaForm);
-
-  var overlayMediaFile = overlayMediaFormData.get("overlayMedia");
-  var mediaFile = mediaFormData.get("media");
-
-  console.log("overlayMediaFile", overlayMediaFile);
-  console.log("mediaFile", mediaFile);
-
-  // Display the key/value pairs
-  console.log("Sart logging");
-  for (var pair of formData.entries()) {
-    console.log(pair[0] + ", " + pair[1]);
-  }
-
-  var postResponse = await postMedia(formData);
-
-  var postJson = await postResponse.json();
-
-  var id = postJson.containerId;
-
-  //TODO: Extract this to a function like setContainerID()
-  containerId = id;
-  publishButton.style.display = "";
-  //
-
-  var response = await getServeMedia(id);
-
-  var mimeType = await response.headers.get("Content-type");
-
-  var myBlob = await response.blob();
-
-  var objectURL = URL.createObjectURL(myBlob);
-
-  var isVideo = checkIsVideo(myBlob);
-
-  if (isVideo) {
-    outputVideo.src = objectURL;
-  } else {
-    photo.src = objectURL;
-  }
-});
-
-cancelButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  /**
-   * @type {HTMLInputElement}
-   */
-
-  //TODO: Extract this to a function like setContainerID()
-  containerId = null;
-
-  var fileInput = document.getElementById("fileInput");
-  fileInput.value = "";
-
-  photo.src = "";
-  photo.style.display = "none";
-
-  outputVideo.src = "";
-  outputVideo.style.display = "none";
-
-  cameraPreview.style.display = "";
+  setContainerId(null);
+  setMedia(null);
 });
 
 //Stop camera preview. This should be done when the user leaves the page.
 window.addEventListener("beforeunload", () => {
-  if (video.srcObject) {
-    let stream = video.srcObject;
+  if (videoPreview.srcObject) {
+    let stream = videoPreview.srcObject;
     let tracks = stream.getTracks();
 
     tracks.forEach((track) => {
       track.stop();
     });
 
-    video.srcObject = null;
+    videoPreview.srcObject = null;
   }
 });
 
-videoButton.addEventListener(
+document.querySelector("#videoButton").addEventListener(
   "click",
   async (ev) => {
     if (isRecording == false) {
-      var result = mediaService.startRecording(video.srcObject, 5000);
-      isRecording = true;
-      videoButton.innerText = "Stop recording";
+      var result = mediaService.startRecording(videoPreview.srcObject, 5000);
+      setIsRecording(true);
       var recordedChunks = await result;
-      console.log("Recording finished");
-
       let recordedBlob = new Blob(recordedChunks, { type: "video/mp4" });
-      formData.append("media", recordedBlob);
-      var url = URL.createObjectURL(recordedBlob);
-      console.log("url", url);
-
-      outputVideo.src = url;
-      outputVideo.style.display = "";
-      cameraPreview.style.display = "none";
-      videoButton.innerText = "Start recording";
+      setMedia(recordedBlob);
+      setIsRecording(false);
     } else {
-      console.log("about to call stop3");
       mediaService.stopRecording();
-      isRecording = false;
-      videoButton.innerText = "Start recording";
+      setIsRecording(false);
     }
     ev.preventDefault();
   },
   false
 );
 
-photoButton.addEventListener(
+document.querySelector("#photoButton").addEventListener(
   "click",
   async (ev) => {
-    const blob = await mediaService.takepicture(video.srcObject);
-    var objectURL = URL.createObjectURL(blob);
-
-    photo.setAttribute("src", objectURL);
-    cameraPreview.style.display = "none";
-    photo.style.display = "";
-    formData.append("media", blob);
+    const blob = await mediaService.takepicture(videoPreview.srcObject);
+    setMedia(blob);
     ev.preventDefault();
   },
   false
